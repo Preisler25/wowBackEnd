0590bd143ae6ae502078dc39c33cd5b7488523e9
Preisler <preisler.andras@gmail.com>
2023-05-17 18:13:51 +0200

    main

--- server.js
+++ server.js
@@ -2,17 +2,35 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 let port = 3000;
+let { createPool } = require('mysql');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+let pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wow'
+});
+
+let dbGet = (sql) => {
+    pool.query(sql, (err, result) => {
+        if (err) throw err;
+        return result;
+    });
+}
 
 app.get('/login', (req, res) => {
     let username = req.query.username;
     let password = req.query.password;
 
+    let sql = `SELECT * FROM users WHERE user_name = '${username}' AND password = '${password}'`;
+    console.log(sql);
+    res = dbGet(sql);
+    print(res);
     if (username == 'admin' && password == 'admin') {
-        res.json({status: true, email: "email", best_time: 100, games_played: 100, miss_clicked: 10});
+        res.json({status: true, email: "email", best_time: 100, games_played: 100, missed_clicks: 10});
     }
     else {
         res.json({status: false, message: 'Login failed'});
